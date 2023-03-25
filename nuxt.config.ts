52cf8fce343dcdc82548f7c2325806494a892f4c
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2023-03-25 15:55:11 +0100

    done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      charset: "UTF-8",
+      title: "Tourist app",
+      viewport: "width=device-width, initial-scale=1.0",
+    },
+  },
+  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
+});
